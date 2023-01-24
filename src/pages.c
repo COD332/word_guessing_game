1d819c8077b82491f3b6a9fb5875a2437bcee777
Alireza <talk32@outlook.com>
2023-01-24 20:51:11 +0330

    main page menu fanction go to the src/pages.c

---
+++ src/pages.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+int main_page_menu(void)
+{
+    // main page menu
+
+    delay(2000);
+    printf("\n   > 1.Login");
+    delay(500);
+    printf("\n   > 2.Signup");
+    delay(500);
+    printf("\n   > 3.Exit");
+    delay(2000);
+
+    while (1)
+    {
+        char key = getch();
+
+        if (key == 72)
+        {
+            if (get_cursor_x() == 2)
+            {
+                continue;
+            }
+
+            cursor_up(1);
+        }
+
+        if (key == 80)
+        {
+            if (get_cursor_x() == 4)
+            {
+                continue;
+            }
+            cursor_down(1);
+        }
+
+        if (key == 13)
+        {
+            int curser_x_pos = get_cursor_x();
+
+            if (curser_x_pos == 4)
+            {
+                // user select exit option
+                return 3;
+            }
+            else if (curser_x_pos == 3)
+            {
+                // user select signup option
+                return 2;
+            }
+            else if (curser_x_pos == 2)
+            {
+                // user select login option
+                return 1;
+            }
+        }
+    }
+}
